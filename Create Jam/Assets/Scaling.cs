c58a6cbec962b7ff0589d403f3fc78154a0c0098
Grovhub <Grovhub@github.com>
2018-11-17 15:36:56 +0100

    Scaling player
    Downscaling of player in correlation of infection

---
+++ Create Jam/Assets/Scaling.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaling : MonoBehaviour
+{
+    public Vector3 temp;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void newoffset()
+    {
+
+        if (InfectionCounter.currentInfection < 30)
+        {
+            temp.x = 1;
+            temp.y = 2F;
+            temp.z = 1F;
+        }
+        //we store scale of this transform in temporary variable
+
+        if (InfectionCounter.currentInfection >= 70)
+        {
+            //We change the values for this saved variable (not actual transform scale)
+            temp.x = 0;
+            temp.y = 1.1F;
+            temp.z = 0;
+        }
+        if (InfectionCounter.currentInfection >= 50 && InfectionCounter.currentInfection < 70)
+        {
+            //We change the values for this saved variable (not actual transform scale)
+            temp.x = 0;
+            temp.y = 1.4F;
+            temp.z = 0;
+        }
+        if (InfectionCounter.currentInfection >= 30 && InfectionCounter.currentInfection < 50)
+        {
+            //We change the values for this saved variable (not actual transform scale)
+            temp.x = 0;
+            temp.y = 1.7F;
+            temp.z = 0;
+        }
+        //We assign temp variable back to transform scale
+        transform.localScale = temp;
+
+    }
+}
--- Create Jam/Assets/Scripts/CameraControlsV2.cs
+++ Create Jam/Assets/Scripts/CameraControlsV2.cs
@@ -8,7 +8,7 @@ public class CameraControlsV2 : MonoBehaviour {
     public Vector3 offset;
     public float mouseSensitivity;
     public float smoothFollow = 0.2f;
-
+    public Vector3 temp;
     float xAxisClamp = 0.0f;
 	void Awake () {
         Cursor.lockState = CursorLockMode.Locked;
@@ -18,7 +18,7 @@ public class CameraControlsV2 : MonoBehaviour {
 	void Update () {
         rotateCamera();
         moveCamera();
-
+        newoffset();
     }
 
     void rotateCamera()
@@ -44,8 +44,22 @@ public class CameraControlsV2 : MonoBehaviour {
         transform.rotation = Quaternion.Euler(targetRotCam);
         playerBody.transform.rotation = Quaternion.Euler(targetRotBody);
     }
+    void newoffset()
+    {
+        if (InfectionCounter.currentInfection > InfectionCounter.previouInfection)
+        {
+            temp.y -= 0.0001F;
+
+            InfectionCounter.previouInfection = InfectionCounter.currentInfection;
+        }
+     
+        //We assign temp variable back to transform scale
+        transform.localScale = temp;
+        offset = new Vector3(0, temp.y, 0);
+    }
     void moveCamera()
     {
+        
         Vector3 targetCamPos = playerBody.position + offset;
         transform.position = Vector3.Slerp(transform.position, targetCamPos, smoothFollow);
     }
