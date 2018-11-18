c7631a87adf206011cb12b9fdacf45eada1a7030
Alexcool3 <Alex33@live.dk>
2018-11-18 10:28:45 +0100

    Audio Setup

---
+++ Create Jam/Assets/Scripts/Audio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour {
+
+    
+    public AudioClip musicOn; // Contains/ Holds the audio clip 
+    public AudioSource musicSource; // Controls the audio clip
+
+    private void Start()
+    {
+        playSound();
+    }
+
+    void playSound()
+    {
+        musicSource.clip = musicOn; // Assigns the AudioSource clip to the AudioClip
+        musicSource.PlayOneShot(musicOn); // Plays the audio clip
+    }
+    void stopSound()
+    {
+        musicSource.Stop();
+    }
+}
